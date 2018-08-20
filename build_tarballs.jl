b8344cd81a67629da1feca25c9ef8016515ca3e6
Jan Strube <jan.strube@gmail.com>
2018-08-19 21:56:43 -0700

    removing some less relevant architectures

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -24,12 +24,10 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, :glibc),
     Linux(:x86_64, :glibc),
     Linux(:armv7l, :glibc, :eabihf),
     Linux(:powerpc64le, :glibc),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
+    MacOS(:x86_64)
 ]
 
 # The products that we will ensure are always built
