8a1ad368479afcda0dd48d82541df6b3ec6701b3
Jan Strube <jan.strube@gmail.com>
2018-09-28 22:10:17 -0700

    using pre-release version of the source tarball

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"0.1.0"
 
 # Collection of sources required to build FastJetBuilder
 sources = [
-    "http://www.fastjet.fr/repo/fastjet-3.3.1.tar.gz" =>
-    "76bfed9b87e5efdb93bcd0f7779e27427fbe38e05fe908c2a2e80a9ca0876c53",
+    "http://fastjet.fr/repository/snapshots/fastjet-3.3.2-devel-20180927-rev4379.tar.gz" =>
+    "6e8aca6d15a647fcc4a2bda6144a40f5b5ea203a459a5893263d1978b87c6402",
 
 ]
 
@@ -17,7 +17,7 @@ sources = [
 # because the libs don't have their dependencies specified properly, 
 # we're hacking the libs together into a single one for the time being
 script = raw"""
-cd $WORKSPACE/srcdir/fastjet-3.3.1/
+cd $WORKSPACE/srcdir/fastjet-*/
 ./configure --prefix=$prefix --host=$target
 make
 make install
@@ -27,8 +27,9 @@ cd ${prefix}/lib
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, libc=:glibc),
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8)),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc4)),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
 ]
 
 #platforms = expand_gcc_versions(platforms)
