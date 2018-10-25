fa75a85b62a7dc6f677126ceaf8c601347ac8bce
Jan Strube <jan.strube@gmail.com>
2018-10-24 23:18:51 -0500

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -29,6 +29,8 @@ platforms = [
     Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc4)),
     Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7)),
     Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8)),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7, :cxx11)),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8, :cxx11)),
 ]
 
 platforms = expand_gcc_versions(platforms)
